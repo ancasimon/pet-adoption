46034a5e076d30ad3df4fa54f2ba764548c5f78b
Anca Simon <ancarsimon@gmail.com>
2020-01-22 18:25:43 -0600

    added pets

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 console.log("Hello, Simon Pet Adoption Agency!");
+
+
+const available_pets = [
+    {
+    Image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/afghan-hound-1550810725.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    Name: "Harry, the Afghan Hound",
+    Color: "tan",
+    Special Skill: "loves hugs",
+    Type of Pet: "Dog"
+    },
+    {
+    'Image:' "https://cdn1-www.cattime.com/assets/uploads/2012/03/1-Maine-Coon-Cat-breed.jpg",
+    Name: "Katahdin, the Maine Coon Cat",
+    Color: "black and white",
+    Special Skill: "kid-friendly",
+    Type of Pet: "Cat"
+    },
+    {
+    Image: "https://cdn2-www.cattime.com/assets/uploads/2012/07/siamese-cat-breed.jpg",
+    Name: "Finny, the Siamese Cat",
+    Color: "brown and tan",
+    Special Skill: "super social",
+    Type of Pet: "Cat"
+    },
+    {
+    Image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bernese-mountain-dog-1550810725.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    Name: "Old Guard, the Bernese Mountain Dog",
+    Color: "black and tan",
+    Special Skill: "protector",
+    Type of Pet: "Dog"
+    },
+    {
+    Image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/boxer-1550810733.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    Name: "Happy, the Boxer",
+    Color: "light brown",
+    Special Skill: "loves to play fetch",
+    Type of Pet: "Dog"
+    },
+    {
+    Image: "https://cdn1-www.cattime.com/assets/uploads/2012/07/bengal-cat-breed-picture.jpg",
+    Name: "Tiger, the Bengal Cat",
+    Color: "brown",
+    Special Skill: "dog-friendly cat",
+    Type of Pet: "Cat"
+    }
+];
+
+console.log(available_pets);
